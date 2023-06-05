e3c068062ce3818361e88547cb303b96300b8cb2
Liudmyla Liaposhchenko <liaposhchenkol@gmail.com>
2023-06-05 21:01:56 +0200

    Add translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
+import translationEn from './locales/en/translation.json';
+import translationCs from './locales/cs/translation.json';
+// i18n.init({
+//   resources: {
+//     en: { translation: translationEn },
+//     cs: { translation: translationCs },
+//   },
+// });
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -20,7 +27,10 @@ const resources = {
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
+    resources: {
+      en: { translation: translationEn },
+      cs: { translation: translationCs },
+    },
     lng: 'cs', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { useTranslation } from 'react-i18next';
 
 export const HomePage = () => {
+  const [isOn, setIsOn] = useState(true);
+  const { t, i18n } = useTranslation();
+
   return (
     <>
       <header>
         <section className="image-section">
-          <h1>Podivej se na Prahu zhora</h1>
+          <h1>{t('title')}</h1>
           <div className="language-buttons">
-            <button className="language-buttons__button ">CZ</button>
-            <button className="language-buttons__button language-buttons__button--selected">
+            <button
+              className={
+                isOn
+                  ? 'language-buttons__button '
+                  : 'language-buttons__button language-buttons__button--selected'
+              }
+              onClick={() => {
+                i18n.changeLanguage('cs');
+                setIsOn(!isOn);
+              }}
+            >
+              CZ
+            </button>
+            <button
+              className={
+                isOn
+                  ? 'language-buttons__button language-buttons__button--selected'
+                  : 'language-buttons__button '
+              }
+              onClick={() => {
+                i18n.changeLanguage('en');
+                setIsOn(!isOn);
+              }}
+            >
               EN
             </button>
           </div>
         </section>
         <section className="introduction">
-          <p>
-            Vítáme v Praze! Máme tady stovky vež, a pravě veže dělají obraz
-            města.
-          </p>
-          <p>Přijďte se na ně podivat a užijte si malebný výhled!</p>
-          <a href="#">Naplanovát si exkurze ↓</a>
+          <p>{t('about')}</p>
+          <p>{t('invite')}</p>
+          <a href="#">{t('go')}</a>
         </section>
       </header>
     </>
